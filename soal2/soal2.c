cbba4c3236a9c8244a988961106ae9ed8e3172af
Jundullah H. R <jundihanif77@gmail.com>
2021-04-16 13:18:45 +0700

    stashing soal2

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -1,10 +1,16 @@
-#include <string.h> // untuk strtok
-
+#include <string.h> // untuk strtok, strlen
+#include <sys/wait.h> // untuk wait()
+// #include <sys/types.h>
+#include <stdlib.h> // untuk exit()
+#include <stdio.h>
+#include <unistd.h>
 
 
 #define DEBUG
 #define HOMEDIR ~
-#define zipdir pets.zip
+#define petszip "pets.zip"
+
+void clean_unused_folder();
 
 int main()
 {
@@ -30,4 +36,72 @@ int main()
      * menggunakan fork dan exec
      * tidak boleh pakai system(), mkdir(), dan rename()
      */
+
+    clean_unused_folder();
+    // exit(0);
+}
+
+void unzip(char *zipdir, char *dest)
+{
+    char *argv[] = {"unzip", "-d", "./result/", "./pets.zip", NULL};
+    if(execvp("unzip", argv) == -1) {
+            perror("unzip(...): Error when calling unzip!");
+            exit(-1);
+    }
+
+    // no need to send signal below because unzip handles the rest.
+}
+
+void clean_unused_folder()
+{
+    /**
+     * 
+     * "*()/" is a pattern that matches all of the subdirectories in the current directory
+     * (* would match all files and subdirectories; the / restricts it to directories). Similarly,
+     * to list all subdirectories under /home/alice/Documents, use ls -d /home/alice/Documents/*()/
+     */
+    
+    int fd[2];
+    pid_t child;
+    char buffer[10];
+
+    pipe(fd);
+    child = fork();
+
+    if (child == 0) {
+        // child
+        // https://stackoverflow.com/questions/7292642/grabbing-output-from-exec
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        char *argv[] = {"ls", "-d", "*/", NULL};
+
+        // execlp("ls", "ls", "-d", "*/", (char *)0);
+        if (execvp(argv[0], argv) == -1) {
+            perror("ls fail");
+        }
+
+        exit(0);
+    }
+
+    close(fd[1]);
+    while (read(fd[0], buffer, sizeof(buffer) - 1) != 0) {
+        // terima semua folder yang gak berguna dari pipe
+        buffer[9] = '\0';
+        printf("Get: %s\n", buffer);
+    }
+    close(fd[0]);
+    wait(NULL);
+
+    // bersihkan folder
+    // strtok
+
+    // rm
+
+    exit(0);
+}
+
+void get_all_filenames_save_to_array(char *dest[])
+{
+    // ls dan strtok
 }

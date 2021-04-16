29823c814146c300d14572ed4acf0f4306cb0132
Jundullah H. R <jundihanif77@gmail.com>
2021-04-16 22:40:53 +0700

    stashing soal2, stage parsing nama file

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -12,7 +12,8 @@
 
 void unzip(const char *zipdir, const char *dest);
 void clean_unused_folder();
-void get_all_filenames_save_to_array(char *dest[]);
+void get_all_filenames_save_to_array(char **dest_pointer[], int *current_dest_size);
+void parse_file_and_act(char *filename);
 
 int main()
 {
@@ -51,9 +52,22 @@ int main()
     wait(NULL);
 
     char **files;
-    get_all_filenames_save_to_array(files);
-    
-    return 1;
+    int *files_count = malloc(sizeof(int));
+    int i;
+
+    get_all_filenames_save_to_array(&files, files_count);
+
+    parse_file_and_act(files[0]);
+
+    // for (i = 0; i < *files_count; ++i) {
+    //     pid_t mover = fork();
+    //     if (mover == 0) {
+            
+    //         exit(0);
+    //     }
+    // }
+
+    return 0;
 }
 
 void unzip(const char *zipdir, const char *dest)
@@ -124,9 +138,9 @@ void clean_unused_folder()
     }
 }
 
-void get_all_filenames_save_to_array(char *dest[])
+void get_all_filenames_save_to_array(char **dest_pointer[], int *current_dest_size)
 {
-    // ls dan strtok
+    char **dest = *dest_pointer;
     /**
      * 
      * "*()/" is a pattern that matches all of the subdirectories in the current directory
@@ -175,13 +189,51 @@ void get_all_filenames_save_to_array(char *dest[])
             // wait(NULL); // tunggu child selesai
         }
         printf("%s\n", filename);
-        pid_t renamer = fork();
-        if (renamer == 0) {
-            execlp("mv", "mv", filename, filename);
+
+        if (*current_dest_size == 0) {
+            dest = malloc(sizeof(char*));
+        } else {
+            dest = realloc(dest, (*current_dest_size + 1) * sizeof(char*));
         }
 
+        dest[*current_dest_size] = filename;
+        ++(*current_dest_size);
+        *dest_pointer = dest;
         filename = strtok(NULL, "\n");
     }
 
-    exit(0);
+    wait(NULL);
+}
+
+void parse_file_and_act(char *filename)
+{
+    /**
+     *      *   (child)
+     *   - pakai strtok dengan delimiter '_' untuk setiap nama agar dapat
+     *     memparse string yang berkaitan dengan peliharaan lain, lalu pakai
+     *     strtok dengan delimiter ';', lalu sesuaikan format
+     *     [jenis peliharaan]:[nama peliharaan]:[umur peliharaan dalam tahun]
+     *     untuk diambil datanya
+     *   - copy file ke folder jenis peliharaan, lalu rename jadi nama peliharaan
+     *   - hapus file
+     *   - masukkan nama peliharaan dan umurnya ke file keterangan.txt di setiap folder jenis peliharaan sesuai format di docs
+     */
+    char *peliharaan = strtok_r(filename, "_", &peliharaan);
+    char *identitas;
+    char *fileres1 = filename;
+
+    // while (peliharaan != NULL) {
+    //     char *jenis = strtok_r(filename, ";", &identitas);
+    //     char *nama = strtok_r(filename, ";", &identitas);
+    //     char *umur = strtok_r(filename, ";", &identitas);
+
+    //     peliharaan = strtok_r(filename, "_", &peliharaan);
+    // }
+    while ((peliharaan = strtok_r(fileres1, "_", &fileres1))) {
+        char *fileres2 = peliharaan;
+        char *jenis = strtok_r(fileres2, ";", &fileres2);
+        char *nama = strtok_r(fileres2, ";", &fileres2);
+        char *umur = strtok_r(fileres2, ";", &fileres2);
+        printf("%s\n", peliharaan);
+    }
 }
